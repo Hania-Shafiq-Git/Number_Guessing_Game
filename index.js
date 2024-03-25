60ac1a5e50259ddd55e0e0c5c87dd3f0eb9e3699
Hania-Shafiq-Git <162552314+Hania-Shafiq-Git@users.noreply.github.com>
2024-03-25 20:38:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "Number",
+        messege: "please guess a number between 1 to 6",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! You are winner");
+}
+else {
+    console.log("You lose");
+}
